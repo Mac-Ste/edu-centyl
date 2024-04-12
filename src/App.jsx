5e8a356c8146d20f2785de7a2fda2f1af97afd62
macste <maciej.sterma@gmail.com>
2024-04-12 13:29:02 +0200

    Theme for material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import * as React from 'react';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Button } from '@mui/material';
+import { Checkbox } from '@mui/material';
+
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#231a4b"
+    },
+    primary: {
+      main: '#fbacb1',
+      light: '#fcc6c9',
+      dark: '#f89aa0',
+      contrastText: '#231a4b',
+    },
+  },
+  typography: {
+    fontFamily: "'Cinzel', serif",
+    fontWeightLight: 500,
+    fontWeightRegular: 600, 
+    fontWeightMedium: 700,
+    fontWeightBold: 800
+  }
+});
+
 export const App = () => {
   return (
     <>
-      <p>hello world</p>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Button variant="contained">Hello</Button>
+        <Checkbox defaultChecked />
+      </ThemeProvider>
+      
     </>
   )
 }
